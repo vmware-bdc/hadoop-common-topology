63195a36df55d5a30958ede3619b826caeb55b6c
JunpingDu <jdu@vmware.com>
2012-05-14 14:00:52 -0700

    Merge copyright updates from Mark

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/balancer/Balancer.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/balancer/Balancer.java
@@ -1,5 +1,6 @@
 /**
  * Portions Copyright 2012 VMware, Inc.
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
