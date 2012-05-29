a3137500b82fa0c1f8b3d243de6c2aaec8c72191
JunpingDu <jdu@vmware.com>
2012-05-29 03:23:26 -0700

    Plugin patch with NodeGroup for core NetworkTopology and related unit test

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/CommonConfigurationKeysPublic.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/CommonConfigurationKeysPublic.java
@@ -64,6 +64,8 @@ public class CommonConfigurationKeysPublic {
   public static final String  NET_TOPOLOGY_NODE_SWITCH_MAPPING_IMPL_KEY =
     "net.topology.node.switch.mapping.impl";
   /** See <a href="{@docRoot}/../core-default.html">core-default.xml</a> */
+  public static final String  NET_TOPOLOGY_WITH_NODEGROUP =
+    "net.topology.with.nodegroup";
   public static final String  NET_TOPOLOGY_CLASS_NAME_KEY =
 	"net.topology.class.name";
   /** See <a href="{@docRoot}/../core-default.html">core-default.xml</a> */
---
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/net/InnerNodeWithNodeGroup.java
@@ -0,0 +1,85 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.net;
+
+class InnerNodeWithNodeGroup extends InnerNode {
+
+	public InnerNodeWithNodeGroup(String name, String location, InnerNode parent,
+			int level) {
+		super(name, location, parent, level);
+	}
+
+	public InnerNodeWithNodeGroup(String name, String location) {
+		super(name, location);
+	}
+
+	public InnerNodeWithNodeGroup(String path) {
+		super(path);
+	}
+
+	@Override
+	boolean isRack() {
+		// it is node group
+		if (getChildren().isEmpty()) {
+			return false;
+		}
+
+		Node firstChild = children.get(0);
+
+		if (firstChild instanceof InnerNode) {
+			Node firstGrandChild = (((InnerNode) firstChild).children).get(0);
+			if (firstGrandChild instanceof InnerNode) {
+				// it is datacenter
+				return false;
+			} else {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Judge if this node represents a node group
+	 * 
+	 * @return true if it has no child or its children are not InnerNodes
+	 */
+	boolean isNodeGroup() {
+	  if (children.isEmpty()) {
+		return true;
+	  }
+
+	  Node firstChild = children.get(0);
+	  if (firstChild instanceof InnerNode) {
+		// it is rack or datacenter
+		return false;
+	  }
+	  return true;
+	}
+
+	@Override
+	protected InnerNode doCreateParentNode(String parentName) {
+		return new InnerNodeWithNodeGroup(parentName, getPath(this), this,
+				this.getLevel() + 1);
+	}
+
+	@Override
+	protected boolean doAreChildrenLeaves() {
+		return isNodeGroup();
+	}
+
+}
